f4a2b55d9ae9ba4dd4a3d16ac80d088eb4a4fe1f
Nghia <nghiadn71@gmail.com>
2022-05-28 16:00:41 +0700

    update

--- src/index.js
+++ src/index.js
@@ -42,6 +42,8 @@ const arrowRender = () => {
     document.querySelector('#footer').innerHTML = '<div>Footer</div>';
 };
 
+const s = () => 2 + 3;
+
 function sum (a, b) {
     return a + b;
 }; // Cú pháp thông thường
@@ -54,3 +56,43 @@ const sum2 = (a, b) => a + b; // Nếu chỉ có return
 
 const display = a => console.log(a); // Nếu chỉ có 1 tham số
 
+// Callback: hàm được truyền vào dưới dạng 1 đối số, và thực thi trong 2 hàm
+
+const abc = (print) => {
+    const result = sum(2,3);
+    // có nhiều cách hiển thị khác nhau, và phải nhận kết quả hiển thị từ result
+    print(result);
+    // display(result);
+    // display1(result);
+};
+
+// abc(display);
+abc(display1);
+
+const loadScript = (src) => {
+    const scriptE = document.createElement('script');
+    scriptE.src = src;
+    scriptE.onload = () => callback();
+};
+
+const khoitaosv = () => {
+    var sinhVien = {};
+}
+
+loadScript('moment.min.js', khoitaosv);
+
+// đếm số chữ cái trong thông tin user
+
+const countString = (string) => string.length;
+const countString2 = (string) => alert(string);
+
+const reciveUser = (user) => {
+    // Lấy ra tên
+    const username = user.name;
+    // nhận tham số truyền vào là 1 trong 2 cách hiển thị tên
+    callback(username);
+    // countString(username);
+};
+
+reciveUser({name: 'nghiadn71'}, countString);
+reciveUser({name: 'nghiadn72'}, countString2);
